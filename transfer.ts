6a57e03466774a25dcc549318914d3bf9d4d5f0f
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-10-09 09:02:35 +0530

    completed transfer all lamports

--- transfer.ts
+++ transfer.ts
@@ -14,17 +14,33 @@ const connection = new Connection("https://api.devnet.solana.com");
 
 (async () => {
   try {
+    const balance = await connection.getBalance(keypair.publicKey);
     const transaction = new Transaction().add(
       SystemProgram.transfer({
         fromPubkey: keypair.publicKey,
         toPubkey: toWallet,
-        lamports: LAMPORTS_PER_SOL / 100,
+        lamports: balance,
       })
     );
     transaction.recentBlockhash = (
       await connection.getLatestBlockhash("confirmed")
     ).blockhash;
     transaction.feePayer = keypair.publicKey;
+    const fee =
+      (
+        await connection.getFeeForMessage(
+          transaction.compileMessage(),
+          "confirmed"
+        )
+      ).value || 0;
+    transaction.instructions.pop();
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: keypair.publicKey,
+        toPubkey: toWallet,
+        lamports: balance - fee,
+      })
+    );
     const signature = await sendAndConfirmTransaction(connection, transaction, [
       keypair,
     ]);
@@ -36,4 +52,6 @@ const connection = new Connection("https://api.devnet.solana.com");
   }
 })();
 
-//Transaction - https://explorer.solana.com/tx/4p4ZopoZHLMazd5rp53nAnYiHBZPM2DtzAECtVSXnjw59LhAVcZEmkn15cbuDoByaeB9h4hdv7au3PG6LtfHj2ZD?cluster=devnet
+//Transaction 0.01 sol - https://explorer.solana.com/tx/4p4ZopoZHLMazd5rp53nAnYiHBZPM2DtzAECtVSXnjw59LhAVcZEmkn15cbuDoByaeB9h4hdv7au3PG6LtfHj2ZD?cluster=devnet
+
+//Transaction complete drain - https://explorer.solana.com/tx/562werEPLo4p1L3JhHZSjz84uqQgPodTRQScmpN5mAe8dKZp56Cf4CfvLdMGAyZWqBzViuhpakB6tbwk54kxYxLT?cluster=devnet
